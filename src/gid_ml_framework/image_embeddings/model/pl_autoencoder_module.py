78ba445ef717e0eaa0420cd0a857843859e2fb79
mmadej <michal.madej@getindata.com>
2022-07-18 09:19:25 +0200

    validation split, and saving matplotlib artifacts to mlflow

--- src/gid_ml_framework/image_embeddings/model/pl_autoencoder_module.py
+++ src/gid_ml_framework/image_embeddings/model/pl_autoencoder_module.py
@@ -1,5 +1,13 @@
-from torch import optim, nn, utils, Tensor
+from torch import optim, nn
 import pytorch_lightning as pl
+import mlflow
+import matplotlib.pyplot as plt
+import numpy as np
+from kedro_mlflow.config import kedro_mlflow_config
+
+# it's needed, so matplotlib artifacts are saved together with the mlflow run
+mlflow.set_experiment(experiment_name=kedro_mlflow_config.get_mlflow_config().tracking.experiment.name)
+
 
 class LitAutoEncoder(pl.LightningModule):
     def __init__(self, encoder, decoder):
@@ -12,13 +20,24 @@ class LitAutoEncoder(pl.LightningModule):
         return embedding
 
     def training_step(self, batch, batch_idx):
-        x, y = batch
+        x, _ = batch
         x = x.view(x.size(0), -1)
         z = self.encoder(x)
         x_hat = self.decoder(z)
         loss = nn.functional.mse_loss(x_hat, x)
         self.log("train_loss", loss, on_epoch=True)
         return loss
+
+    def validation_step(self, batch, batch_idx):
+        x, _ = batch
+        x = x.view(x.size(0), -1)
+        z = self.encoder(x)
+        x_hat = self.decoder(z)
+        loss = nn.functional.mse_loss(x_hat, x)
+        self.log("val_loss", loss, on_epoch=True)
+        return {'val_loss': loss,
+                'x_hat': x_hat,
+                'x': x}
 
     def configure_optimizers(self):
         optimizer = optim.Adam(self.parameters(), lr=1e-3)
@@ -28,3 +47,14 @@ class LitAutoEncoder(pl.LightningModule):
         x, y = batch
         embedding = self.encoder(x)
         return embedding, y
+
+    def validation_epoch_end(self, outputs):
+        results = outputs[0]
+        fig, axarr = plt.subplots(4, 2)
+        for i in range(4):
+            img_reconstructed = results['x_hat'][i, :].reshape(3, 128, 128)
+            img_original = results['x'][i, :].reshape(3, 128, 128)
+            axarr[i, 0].imshow(np.transpose(img_reconstructed.numpy(), (1, 2, 0)))
+            axarr[i, 1].imshow(np.transpose(img_original.numpy(), (1, 2, 0)))
+        mlflow.log_figure(plt.gcf(), f'reconstructed/img_reconstructed_{self.current_epoch}.png')
+        plt.close()
