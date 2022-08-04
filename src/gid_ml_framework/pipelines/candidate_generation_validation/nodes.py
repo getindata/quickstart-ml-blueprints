9c1da38a991e5e7aee34f3f65ec8cb608ccbca48
mmadej <michal.madej@getindata.com>
2022-08-04 11:57:00 +0200

    refactoring and docstrings

--- src/gid_ml_framework/pipelines/candidate_generation_validation/nodes.py
+++ src/gid_ml_framework/pipelines/candidate_generation_validation/nodes.py
@@ -1,22 +1,50 @@
 import pandas as pd
+import numpy as np
 import logging
-from typing import List
+from typing import List, Set
 import mlflow
 
 
 log = logging.getLogger(__name__)
 
 def _get_candidate_column_names(candidates: pd.DataFrame) -> List:
+    """Returns all column names for different candidate generation methods.
+
+    Args:
+        candidates (pd.DataFrame): candidates
+
+    Returns:
+        List: different candidate generation methods
+    """
     col_list = [col for col in list(candidates.columns) if col != 'customer_id']
     return col_list
 
 def _get_unique_transactions(transactions: pd.DataFrame) -> pd.DataFrame:
+    """Removes repeated purchases from transactions
+
+    Args:
+        transactions (pd.DataFrame): transactions
+
+    Returns:
+        pd.DataFrame: filtered transactions
+    """
     log.info(f'Transactions shape before removing duplicates (repeated purchases): {transactions.shape}')
     transactions = transactions[['customer_id', 'article_id']].drop_duplicates()
     log.info(f'Transactions shape after removing duplicates: {transactions.shape}')
     return transactions
 
 def _get_recall(candidates: pd.DataFrame, val_transactions: pd.DataFrame, candidate_col: str) -> float:
+    """Calculates recall for given candidate generation method. Candidate method column should have 
+    list-like data.
+
+    Args:
+        candidates (pd.DataFrame): candidates
+        val_transactions (pd.DataFrame): validation transactions (unseen) on which to calculate recall
+        candidate_col (str): column name pointing to candidate generation method
+
+    Returns:
+        float: recall, should be between 0 and 1
+    """
     exploded_candidates_df = candidates[['customer_id', candidate_col]].explode(candidate_col)
     exploded_candidates_df['recall'] = 1
     val_transactions = val_transactions.merge(
@@ -28,32 +56,44 @@ def _get_recall(candidates: pd.DataFrame, val_transactions: pd.DataFrame, candid
     recall = val_transactions['recall'].fillna(0).mean()
     return recall
 
-def _concatenate_all_lists(*args) -> List:
+def _concatenate_all_lists(row: np.ndarray, no_cols: int) -> Set:
+    """Helper function to `np.apply_along_axis`.
+    Concatenates all candidates from multiple methods. It returns set to keep only unique values.
+
+    Args:
+        row (np.ndarray): row, which can be indexed
+        no_cols (int): number of cols to aggregate
+
+    Returns:
+        Set: unique items from all columns
+    """
     articles_set = set()
-    for arg in args:
-        if arg is None:
+    for i in range(no_cols):
+        if row[i] is None:
             continue
-        new_items = set(arg)
-        articles_set |= new_items
-    return list(articles_set)
+        articles_set |= set(row[i])
+    return articles_set
 
 def log_retrieval_recall(candidates: pd.DataFrame, val_transactions: pd.DataFrame) -> None:
+    """Logs recall to MLflow for all candidate generation methods and jointly.
+
+    Args:
+        candidates (pd.DataFrame): candidates
+        val_transactions (pd.DataFrame): validation transactions
+    """
     candidates_cols_list = _get_candidate_column_names(candidates)
     val_transactions = _get_unique_transactions(val_transactions)
+    # all candidates
+    candidates['all_candidates'] = np.apply_along_axis(
+        _concatenate_all_lists,
+        axis=1,
+        arr=candidates[candidates_cols_list].values,
+        no_cols=len(candidates_cols_list)
+    )
+    candidates['all_candidates'] = candidates['all_candidates'].apply(list)
+    candidates_cols_list = candidates_cols_list + ['all_candidates']
     for candidate_col in candidates_cols_list:
         log.info(f'Calculating recall for {candidate_col}')
         recall = _get_recall(candidates, val_transactions, candidate_col)
         mlflow.log_metric(f'{candidate_col}_recall', recall)
-    # log recall for all candidates
-    # HARD CODED - dunno yet how to make it general
-    log.info(f'Calculating recall for all candidates')
-    candidates['all_candidates'] = candidates.apply(lambda x: _concatenate_all_lists(x.global_articles,
-                                             x.segment_articles,
-                                             x.previously_bought,
-                                             x.previously_bought_prod_name,
-                                             x.closest_image_embeddings,
-                                             x.closest_text_embeddings
-                                            ), axis=1)
-    recall = _get_recall(candidates, val_transactions, 'all_candidates')
-    mlflow.log_metric(f'all_candidates_recall', recall)
     
