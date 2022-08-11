8b4982a6c24fad5f52c30302f243051f2fc0f94c
mmadej <michal.madej@getindata.com>
2022-08-11 12:35:07 +0200

    created manual feature engineering kedro pipeline

---
+++ src/gid_ml_framework/pipelines/manual_feature_engineering/pipeline.py
@@ -0,0 +1,10 @@
+"""
+This is a boilerplate pipeline 'manual_feature_engineering'
+generated using Kedro 0.17.7
+"""
+
+from kedro.pipeline import Pipeline, node, pipeline
+
+
+def create_pipeline(**kwargs) -> Pipeline:
+    return pipeline([])
