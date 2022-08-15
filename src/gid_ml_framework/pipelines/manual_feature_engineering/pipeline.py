557486253a79ed956eafd9046a77281d61961c76
mmadej <michal.madej@getindata.com>
2022-08-15 10:46:05 +0200

    rewrite static features from jupyter to kedro pipelines

--- src/gid_ml_framework/pipelines/manual_feature_engineering/pipeline.py
+++ src/gid_ml_framework/pipelines/manual_feature_engineering/pipeline.py
@@ -1,10 +1,41 @@
-"""
-This is a boilerplate pipeline 'manual_feature_engineering'
-generated using Kedro 0.17.7
-"""
+from kedro.pipeline import Pipeline, node
+from kedro.pipeline.modular_pipeline import pipeline
 
-from kedro.pipeline import Pipeline, node, pipeline
+from .nodes import create_article_features, create_customer_features, create_customer_product_group_features
 
 
 def create_pipeline(**kwargs) -> Pipeline:
-    return pipeline([])
+    return pipeline(
+        [
+            node(
+                func=create_article_features,
+                name="create_article_features_node",
+                inputs=[
+                    "transactions_sample",
+                    ],
+                outputs="manual_article_features",
+            ),
+            node(
+                func=create_customer_features,
+                name="create_customer_features_node",
+                inputs=[
+                    "transactions_sample",
+                    "articles_sample",
+                    "params:manual_feature_engineering.customers.n_days",
+                    ],
+                outputs="manual_customer_features",
+            ),
+            node(
+                func=create_customer_product_group_features,
+                name="create_customer_product_group_features_node",
+                inputs=[
+                    "transactions_sample",
+                    "articles_sample",
+                    ],
+                outputs="manual_customer_prod_group_features",
+            ),
+        ],
+        namespace="manual_feature_engineering",
+        inputs=["transactions_sample", "articles_sample"],
+        outputs=["manual_article_features", "manual_customer_features", "manual_customer_prod_group_features"],
+    )
