9e06b2212168d0449704516bfceaac66182f69f4
mmadej <michal.madej@getindata.com>
2022-08-22 17:50:44 +0200

    ranking: preparing dataset

--- src/gid_ml_framework/pipelines/ranking/pipeline.py
+++ src/gid_ml_framework/pipelines/ranking/pipeline.py
@@ -1,10 +1,58 @@
-"""
-This is a boilerplate pipeline 'ranking'
-generated using Kedro 0.17.7
-"""
+from kedro.pipeline import Pipeline, node
+from kedro.pipeline.modular_pipeline import pipeline
 
-from kedro.pipeline import Pipeline, node, pipeline
+from .nodes import add_article_features, add_customer_features, add_dict_features
 
 
 def create_pipeline(**kwargs) -> Pipeline:
-    return pipeline([])
+    prepare_dataset_pipeline = pipeline(
+        [
+            node(
+                func=add_article_features,
+                name="add_article_features_node",
+                inputs=[
+                    "candidates",
+                    "automated_articles_features",
+                    "manual_article_features",
+                    "params:ranking.data.fill_na_pattern",
+                    ],
+                outputs="candidates_step_1",
+            ),
+            node(
+                func=add_customer_features,
+                name="add_customer_features_node",
+                inputs=[
+                    "candidates_step_1",
+                    "automated_customers_features",
+                    "manual_customer_features",
+                    "params:ranking.data.fill_na_pattern",
+                    ],
+                outputs="candidates_step_2",
+            ),
+            node(
+                func=add_dict_features,
+                name="add_dict_features_node",
+                inputs=[
+                    "candidates_step_2",
+                    "articles",
+                    "customers",
+                    "params:ranking.data.categorical_cols",
+                    "params:ranking.data.drop_cols",
+                    ],
+                outputs="training_data",
+            ),
+        ],
+        namespace="ranking",
+        inputs=["candidates",
+                "automated_articles_features",
+                "manual_article_features",
+                "automated_customers_features",
+                "manual_customer_features",
+                "articles",
+                "customers",
+                ],
+        outputs="training_data",
+    )
+
+    
+    return prepare_dataset_pipeline
