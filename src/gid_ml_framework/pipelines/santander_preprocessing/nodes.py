bea9bdc7846752d18a22dfe38c80a3aa3c959c7d
michal.stawikowski <michal.stawikowski@getindata.com>
2022-08-23 17:00:06 +0200

    adding nodes to santander_to_act pipeline

--- src/gid_ml_framework/pipelines/santander_preprocessing/nodes.py
+++ src/gid_ml_framework/pipelines/santander_preprocessing/nodes.py
@@ -15,7 +15,7 @@ from gid_ml_framework.extras.datasets.chunks_dataset import (
 
 pd.options.mode.chained_assignment = None
 log = logging.getLogger(__name__)
-# Overwriting load method because of chunksize bug
+# Overwriting load method because of chunksize bug in Kedro < 0.18
 CSVDataSet._load = _load
 
 
@@ -227,70 +227,3 @@ def impute_santander(santander_df: Iterator[pd.DataFrame],
     log.info(f'Columns with missing values: \
             {df.columns[df.isnull().any()].tolist()}')
     return df
-
-
-def _status_change(x: pd.Series) -> str:
-    """Based on difference of the following rows create label which indicates
-    if given product was added, dropped or maintained in comparison with last 
-    month for given customer.
-
-    Args:
-        x (pd.DataFrame): imputed santander train dataframe
-
-    Returns:
-        str: target label - added/dropped/maintained
-    """
-    diffs = x.diff().fillna(0)
-    # First occurrence is considered as "Maintained"
-    label = ["Added" if i == 1 \
-         else "Dropped" if i == -1 \
-         else "Maintained" for i in diffs]
-    return label
-
-
-def target_processing_santander(input_train_df: Iterator[pd.DataFrame],
-                                input_val_df: Iterator[pd.DataFrame]) -> Tuple:
-    """Preprocess target columns to focus on products that will be bought
-    in the next month
-
-    Args:
-        input_train_df (Iterator[pd.DataFrame]): imputed santander train
-        dataframe
-        input_val_df (Iterator[pd.DataFrame]): imputed santander validation 
-        dataframe
-
-    Returns:
-        Tuple: processed train and validation dataframes
-    """
-    train_df = _concat_chunks(input_train_df)
-    val_df = _concat_chunks(input_val_df)
-    train_len = len(train_df)
-    df = pd.concat([train_df, val_df])
-    feature_cols = df.iloc[:1,].filter(regex="ind_+.*ult.*").columns.values
-    # Create auxiliary column
-    unique_months = (pd.DataFrame(pd.Series(df.fecha_dato.unique())
-                     .sort_values()).reset_index(drop=True))
-    # Start with month 1, not 0 to match what we already have
-    unique_months["month_id"] = pd.Series(range(1, 1+unique_months.size))
-    unique_months["month_next_id"] = 1 + unique_months["month_id"]
-    unique_months.rename(columns={0:"fecha_dato"}, inplace=True)
-    df = pd.merge(df,unique_months, on="fecha_dato")
-    # Apply status change labeling
-    df.loc[:, feature_cols] = (df.loc[:, [i for i in feature_cols]
-                               + ["ncodpers"]].groupby("ncodpers")
-                               .transform(_status_change))
-    # Can be done faster but some tweaks needed 
-    # df = df.sort_values(['ncodpers', 'fecha_dato']).reset_index(drop=True)
-    # s = df.loc[:, [i for i in feature_cols] + ["ncodpers"]]
-    # df.loc[:, feature_cols] = (s.loc[:, [i for i in feature_cols]].diff()
-    #                            .where(s.duplicated(["ncodpers"],
-    #                            keep='first'))
-    #                            ).fillna(0).transform(_status_change2)
-    df = df.sort_values(['fecha_dato']).reset_index(drop=True)
-
-    log.info(f'Sum of number of newly added products: \
-             {df.eq("Added").sum().sum()}')
-
-    train_df = df.iloc[:train_len, :]
-    val_df = df.iloc[train_len:, :]
-    return (train_df, val_df)
