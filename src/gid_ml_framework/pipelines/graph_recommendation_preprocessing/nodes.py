beee64353d480a93b173cad587981692190c3535
michal.stawikowski <michal.stawikowski@getindata.com>
2022-08-29 17:14:59 +0200

    wip

--- src/gid_ml_framework/pipelines/graph_recommendation_preprocessing/nodes.py
+++ src/gid_ml_framework/pipelines/graph_recommendation_preprocessing/nodes.py
@@ -25,7 +25,11 @@ def concat_train_val(
 
     Args:
         train_df (pd.DataFrame): transactions train dataframe
+<<<<<<< HEAD
         val_df (pd.DataFrame): transaction val dataframe
+=======
+        val_df (pd.DataFrame): transaction val datafraame
+>>>>>>> wip
 
     Returns:
         pd.DataFrame: concatenated transactions dataframe
