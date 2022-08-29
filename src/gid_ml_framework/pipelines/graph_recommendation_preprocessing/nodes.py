326f19d84359dfbff8288b1dc4dfca97a1840a68
michal.stawikowski <michal.stawikowski@getindata.com>
2022-08-29 17:14:59 +0200

    wip

--- src/gid_ml_framework/pipelines/graph_recommendation_preprocessing/nodes.py
+++ src/gid_ml_framework/pipelines/graph_recommendation_preprocessing/nodes.py
@@ -26,11 +26,13 @@ def concat_train_val(train_df: pd.DataFrame, val_df: pd.DataFrame, date_column: 
 
     Args:
         train_df (pd.DataFrame): transactions train dataframe
-        val_df (pd.DataFrame): transaction val datafreame
+        val_df (pd.DataFrame): transaction val datafraame
 
     Returns:
         pd.DataFrame: concatenated transactions dataframe
     """
+    train_df = _concat_chunks(train_df)
+    val_df = _concat_chunks(val_df)
     concat_df = pd.concat([train_df, val_df]).reset_index(drop=True)
     concat_df.loc[:, 'time'] = concat_df.loc[:, date_column].values.astype(np.int64) // 10 ** 9
     concat_df.drop(date_column, axis=1, inplace=True)
@@ -38,7 +40,20 @@ def concat_train_val(train_df: pd.DataFrame, val_df: pd.DataFrame, date_column: 
     return concat_df
 
 
-def map_users_and_items(train_df: pd.DataFrame, val_df: pd.DataFrame) -> Tuple:
+def map_users_and_items(transactions_df: pd.DataFrame, customers_df: pd.DataFrame, articles_df: pd.DataFrame) \
+     -> Tuple[pd.DataFrame, pd.DataFrame, pd.DataFrame]:
     """Map users and items ids to consecutive integers.
 
+    Args:  
+        transactions_df (pd.DataFrame): concatenated train and val transactions dataframes
+
+    Returns:
+        Tuple: tuple of dataframes including original dataframe with mapping applied and mappings for users and items
     """
+    transactions_df = _concat_chunks(transactions_df)
+    customers_df = _concat_chunks(customers_df)
+    articles_df = _concat_chunks(articles_df)
+    log.info(f"Transactions dataframe shape: {transactions_df.shape}")
+    # Users mapping
+    customers_df = customers_df.loc[:, 'customer_id'].sort_values('customer_id').reset_index(drop=True)
+    
