8c0f1776e19cf3e09b05545586ee5af8ccad6823
mmadej <michal.madej@getindata.com>
2022-09-01 12:02:32 +0200

    updated kedro version

--- src/gid_ml_framework/hooks.py
+++ src/gid_ml_framework/hooks.py
@@ -4,7 +4,6 @@ from typing import Any, Dict, Iterable, Optional
 from kedro.config import ConfigLoader
 from kedro.framework.hooks import hook_impl
 from kedro.io import DataCatalog
-from kedro.versioning import Journal
 
 
 class ProjectHooks:
@@ -21,8 +20,7 @@ class ProjectHooks:
         credentials: Dict[str, Dict[str, Any]],
         load_versions: Dict[str, str],
         save_version: str,
-        journal: Journal,
     ) -> DataCatalog:
         return DataCatalog.from_config(
-            catalog, credentials, load_versions, save_version, journal
+            catalog, credentials, load_versions, save_version
         )
