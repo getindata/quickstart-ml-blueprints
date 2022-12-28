38d30343bb6a50452267bbdbb56ee0aa11ca3ca5
piotr.chaberski <piotr.chaberski@getindata.com>
2022-12-28 14:04:08 +0100

    subset concatenation added

--- ga4-mlops/src/ga4_mlops/pipelines/data_preprocessing/nodes.py
+++ ga4-mlops/src/ga4_mlops/pipelines/data_preprocessing/nodes.py
@@ -9,14 +9,39 @@ import pandas as pd
 logger = logging.getLogger(__name__)
 
 
-def get_and_aggregate_data(ga4_data: pd.DataFrame):
-    """_summary_
+def get_and_aggregate_data(ga4_data: pd.DataFrame) -> pd.DataFrame:
+    """Select, aggregate and sample GA4 data using parametrized SQL query.
 
     Args:
-        ga4_data (pd.DataFrame): _description_
+        ga4_data (pd.DataFrame): dataset (train, valid, test or prediction)
 
     Returns:
-        _type_: _description_
+        pd.DataFrame: initially prepared data
     """
     logger.info(f"Table downloaded, shape: {ga4_data.shape}")
-    logger.info(ga4_data.head(5))
+
+    return ga4_data
+
+
+def concatenate_train_valid_test(
+    df_train: pd.DataFrame, df_valid: pd.DataFrame, df_test: pd.DataFrame
+) -> pd.DataFrame:
+    """
+
+    Args:
+        df_train (pd.DataFrame): training subset
+        df_valid (pd.DataFrame): validation subset
+        df_test (pd.DataFrame): test subset
+
+    Returns:
+        pd.DataFrame: combined subsets with subset labels
+    """
+    logger.info("Combining and labeling train/valid test subsets...")
+
+    df_train["subset"] = "train"
+    df_valid["subset"] = "valid"
+    df_test["subset"] = "test"
+
+    df_train_valid_test = pd.concat([df_train, df_valid, df_test])
+
+    return df_train_valid_test
--- ga4-mlops/src/ga4_mlops/pipelines/data_preprocessing/pipeline.py
+++ ga4-mlops/src/ga4_mlops/pipelines/data_preprocessing/pipeline.py
@@ -5,7 +5,7 @@ generated using Kedro 0.18.4
 
 from kedro.pipeline import Pipeline, node, pipeline
 
-from .nodes import get_and_aggregate_data
+from .nodes import concatenate_train_valid_test, get_and_aggregate_data
 
 
 def create_pipeline(train: bool = True, **kwargs) -> Pipeline:
@@ -16,7 +16,7 @@ def create_pipeline(train: bool = True, **kwargs) -> Pipeline:
                 name="get_and_aggregate_data_train_node",
                 func=get_and_aggregate_data,
                 inputs=["ga4_data_train"],
-                outputs=None,
+                outputs="df_train",
             ),
         ]
     )
@@ -27,7 +27,7 @@ def create_pipeline(train: bool = True, **kwargs) -> Pipeline:
                 name="get_and_aggregate_data_valid_node",
                 func=get_and_aggregate_data,
                 inputs=["ga4_data_valid"],
-                outputs=None,
+                outputs="df_valid",
             ),
         ]
     )
@@ -38,7 +38,18 @@ def create_pipeline(train: bool = True, **kwargs) -> Pipeline:
                 name="get_and_aggregate_data_test_node",
                 func=get_and_aggregate_data,
                 inputs=["ga4_data_test"],
-                outputs=None,
+                outputs="df_test",
+            ),
+        ]
+    )
+
+    concat_pipeline = pipeline(
+        [
+            node(
+                name="concatenate_train_valid_test_node",
+                func=concatenate_train_valid_test,
+                inputs=["df_train", "df_valid", "df_test"],
+                outputs="df_train_valid_test",
             ),
         ]
     )
@@ -49,12 +60,12 @@ def create_pipeline(train: bool = True, **kwargs) -> Pipeline:
                 name="get_and_aggregate_data_batch_predict_node",
                 func=get_and_aggregate_data,
                 inputs=["ga4_data_batch_predict"],
-                outputs=None,
+                outputs="df_batch_predict",
             ),
         ]
     )
 
     if train:
-        return train_pipeline + valid_pipeline + test_pipeline
+        return train_pipeline + valid_pipeline + test_pipeline + concat_pipeline
     else:
         return batch_predict_pipeline
