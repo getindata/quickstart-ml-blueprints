1da9d8244e1196c8dd4e1a3056d5b6c57eb43705
piotr.chaberski <piotr.chaberski@getindata.com>
2023-01-23 17:07:27 +0100

    added explanations for calibrator

--- ga4-mlops/src/ga4_mlops/pipelines/explanation/pipeline.py
+++ ga4-mlops/src/ga4_mlops/pipelines/explanation/pipeline.py
@@ -22,55 +22,64 @@ def create_pipeline(subset: str, **kwargs) -> Pipeline:
     assert subset in possible_subsets, f"Subset should be one of: {possible_subsets}"
 
     namespace = subset
+    models = ["model", "calibrator"]
 
-    pipeline_template = pipeline(
-        [
-            node(
-                name="sample_data_node",
-                func=sample_data,
-                inputs=["abt", "params:n_obs", "params:sampling_seed"],
-                outputs="abt_sample",
-            ),
-            node(
-                name="calculate_shap_node",
-                func=calculate_shap,
-                inputs=["abt_sample", "stored.model"],
-                outputs="shap_values",
-            ),
-            node(
-                name="create_explanations_node",
-                func=create_explanations,
-                inputs=[
-                    "shap_values",
-                    "abt_sample",
-                    "stored.model",
-                    "params:pdp_top_n",
-                ],
-                outputs=[
-                    "shap_summary_plot",
-                    "feature_importance",
-                    "partial_dependence_plots",
-                ],
-            ),
-        ]
-    )
+    sub_pipelines = []
 
-    main_pipeline = pipeline(
-        pipe=pipeline_template,
-        inputs={
-            "stored.model": "stored.model",
-        },
-        parameters={
-            "n_obs": "n_obs",
-            "sampling_seed": "sampling_seed",
-            "pdp_top_n": "pdp_top_n",
-        },
-        outputs={
-            "shap_summary_plot": "shap_summary_plot",
-            "feature_importance": "feature_importance",
-            "partial_dependence_plots": "partial_dependence_plots",
-        },
-        namespace=namespace,
-    )
+    for model in models:
+        pipeline_template = pipeline(
+            [
+                node(
+                    name="sample_data_node",
+                    func=sample_data,
+                    inputs=["abt", "params:n_obs", "params:sampling_seed"],
+                    outputs="abt_sample",
+                ),
+                node(
+                    name=f"{model}.calculate_shap_node",
+                    func=calculate_shap,
+                    inputs=["abt_sample", f"stored.{model}"],
+                    outputs=f"{model}.shap_values",
+                ),
+                node(
+                    name=f"{model}.create_explanations_node",
+                    func=create_explanations,
+                    inputs=[
+                        f"{model}.shap_values",
+                        "abt_sample",
+                        f"stored.{model}",
+                        "params:pdp_top_n",
+                    ],
+                    outputs=[
+                        f"{model}.shap_summary_plot",
+                        f"{model}.feature_importance",
+                        f"{model}.partial_dependence_plots",
+                    ],
+                ),
+            ]
+        )
+
+        sub_pipeline = pipeline(
+            pipe=pipeline_template,
+            inputs={
+                f"stored.{model}": f"stored.{model}",
+            },
+            parameters={
+                "n_obs": "n_obs",
+                "sampling_seed": "sampling_seed",
+                "pdp_top_n": "pdp_top_n",
+            },
+            outputs={
+                f"{model}.shap_values": f"{model}.shap_values",
+                f"{model}.shap_summary_plot": f"{model}.shap_summary_plot",
+                f"{model}.feature_importance": f"{model}.feature_importance",
+                f"{model}.partial_dependence_plots": f"{model}.partial_dependence_plots",
+            },
+            namespace=namespace,
+        )
+
+        sub_pipelines += [sub_pipeline]
+
+    main_pipeline = sub_pipelines[0] + sub_pipelines[1]
 
     return main_pipeline
