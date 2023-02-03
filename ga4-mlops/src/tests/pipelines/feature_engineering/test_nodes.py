daab9c45ea396e34f4e5b301bbfa5dec82c0b14a
piotr.chaberski <piotr.chaberski@getindata.com>
2023-02-03 15:09:09 +0100

    modified manual feature engineering tests

--- ga4-mlops/src/tests/pipelines/feature_engineering/test_nodes.py
+++ ga4-mlops/src/tests/pipelines/feature_engineering/test_nodes.py
@@ -14,24 +14,11 @@ from ga4_mlops.pipelines.feature_engineering.nodes import (
 class TestEngineerFeatures:
     def test_transformation(self, engineer_features_sample):
         df = engineer_features(engineer_features_sample)
-        expected_df = pd.DataFrame(
-            {
-                "c_visit_start_hour": [0, 7, 11],
-                "c_weekday": [1, 3, 1],
-                "i_full_visitor_id": [
-                    3033910.355860057,
-                    81793309.0616803,
-                    4293031.296243032,
-                ],
-                "i_visit_start_time": [
-                    1611619614341157.0,
-                    1611820704267587.0,
-                    1611661585573344.0,
-                ],
-            }
-        )
+        expected_visit_start_hours = [0, 7, 11]
+        expected_weekdays = [1, 3, 1]
 
-        assert df.sort_index(axis=1).equals(expected_df.sort_index(axis=1))
+        assert df["c_visit_start_hour"].to_list() == expected_visit_start_hours
+        assert df["c_weekday"].to_list() == expected_weekdays
 
 
 class TestExcludeFeatures:
